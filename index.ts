4e574ee9af42c01fe110e1d66f713a043bfb9b73
Felexonyango <felexonyango19@gmail.com>
2022-04-08 18:35:36 +0300

    text

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ import * as dotenv from 'dotenv';
  app.use(cors());
 
  app.use('/',routes)
- 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||5000
+
  app.listen(PORT, ()=> console.log(`Server started at ${PORT}`));
